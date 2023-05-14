2a40b0ff2ad9f4ecad2cd6e971d3e5a330575884
LaptevVV <WLaptew@yandex.ru>
2023-05-14 21:30:43 +0400

    start-go

--- Number.go
+++ Number.go
@@ -1,8 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
+
+	var number int = 30
+	fmt.Println("--------------")
 	fmt.Println("Привет, Анюта!")
+	fmt.Println("--------------")
+	fmt.Println("Задумай число от 1 до", number)
+	fmt.Println("Я его угадаю. ")
+	fmt.Println("--------------")
+	fmt.Println("Я буду задавать вопросы, а ты будешь мне отвечать на клавиатуре. ")
+	fmt.Println("По-английски. ")
+	fmt.Println("Если твой ответ - да,  то набирай на клавиатуре yes ")
+	fmt.Println("Если твой ответ - нет, то набирай на клавиатуре no ")
+	fmt.Println("И потом нажми Enter")
+	fmt.Println("Ты поняла ?")
+
+	// -- пока не работает --
+	answer := "" // -- это строка --
+	fmt.Scanln(answer)
+
+	if answer == "yes" {
+		fmt.Println("\nПопилили!")
+	}
 
 }
