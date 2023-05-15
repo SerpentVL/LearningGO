8649c627daac0a83435da54d9fc62c4b178b6a9c
LaptevVV <WLaptew@yandex.ru>
2023-05-15 08:10:32 +0400

    func-start-is-worked

--- Number.go
+++ Number.go
@@ -2,8 +2,21 @@ package main
 
 import (
 	"fmt"
-	//"os"
 )
+
+func Start() string {
+
+	fmt.Println("Я буду задавать вопросы, а ты будешь мне отвечать на клавиатуре. ")
+	fmt.Println("По-английски. ")
+	fmt.Println("Если твой ответ - да,  то набирай на клавиатуре yes ")
+	fmt.Println("Если твой ответ - нет, то набирай на клавиатуре no ")
+	fmt.Println("И потом нажми Enter")
+	fmt.Println("Ты поняла ?")
+
+	var answer string
+	fmt.Scan(&answer)
+	return answer
+}
 
 func main() {
 
@@ -14,19 +27,19 @@ func main() {
 	fmt.Println("Задумай число от 1 до", number)
 	fmt.Println("Я его угадаю. ")
 	fmt.Println("--------------")
-	fmt.Println("Я буду задавать вопросы, а ты будешь мне отвечать на клавиатуре. ")
-	fmt.Println("По-английски. ")
-	fmt.Println("Если твой ответ - да,  то набирай на клавиатуре yes ")
-	fmt.Println("Если твой ответ - нет, то набирай на клавиатуре no ")
-	fmt.Println("И потом нажми Enter")
-	fmt.Println("Ты поняла ?")
-
 	var answer string
-	fmt.Scan(&answer)
+	answer = Start()
 
 	if answer == "yes" {
 		fmt.Println("--------------")
 		fmt.Println("Погнали!")
+	} else if answer == "no" {
+		fmt.Println("--------------")
+		fmt.Println("До свидания!")
+	} else {
+		fmt.Println("--------------")
+		fmt.Println("Ты ошиблась. Давай попробуем еще раз.")
 	}
-
+	fmt.Println("--------------")
+	fmt.Println("----- КОНЕЦ -----")
 }
