d381c97c55386dbe1a02e7c6b9dc17ba39b2d2f6
LaptevVV <WLaptew@yandex.ru>
2023-05-15 10:12:13 +0400

    version-1.0

--- Number.go
+++ Number.go
@@ -61,6 +61,19 @@ func main() {
 	} else {
 		fmt.Println("--------------")
 		fmt.Println("Ты ошиблась. Давай попробуем еще раз.")
+		answer = Start()
+		if answer == "yes" {
+			fmt.Println("--------------")
+			fmt.Println("Погнали!")
+			Number := Work(number)
+			fmt.Println(Number)
+		} else if answer == "no" {
+			fmt.Println("--------------")
+			fmt.Println("До свидания!")
+		} else {
+			fmt.Println("Ты устала. Отдохни.Потом попробуй еще раз.")
+			fmt.Println("До свидания!")
+		}
 	}
 	fmt.Println("--------------")
 	fmt.Println("----- КОНЕЦ -----")
