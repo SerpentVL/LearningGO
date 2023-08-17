fba69a189a8021aa2b148c04d36f7dbc90a3455e
LaptevVV <WLaptew@yandex.ru>
2023-08-17 12:22:53 +0400

    multiplierr

---
+++ GameGo/Multiplier.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	fmt.Println("----------------")
+	fmt.Println(" Привет, Анюта!")
+	fmt.Println("----------------")
+	fmt.Println("Будем учить таблицу умножения")
+	fmt.Println("---------------")
+	/*
+		var answer string
+		answer = Start()
+
+		if answer == "yes" {
+			fmt.Println("--------------")
+			fmt.Println("Погнали!")
+			Number := Work(number)
+			fmt.Println(Number)
+		} else if answer == "no" {
+			fmt.Println("--------------")
+			fmt.Println("До свидания!")
+		} else {
+			fmt.Println("--------------")
+			fmt.Println("Ты ошиблась. Давай попробуем еще раз.")
+			answer = Start()
+			if answer == "yes" {
+				fmt.Println("--------------")
+				fmt.Println("Погнали!")
+				Number := Work(number)
+				fmt.Println(Number)
+			} else if answer == "no" {
+				fmt.Println("--------------")
+				fmt.Println("До свидания!")
+			} else {
+				fmt.Println("Ты устала. Отдохни.Потом попробуй еще раз.")
+				fmt.Println("До свидания!")
+			}
+		}
+	*/
+	fmt.Println("----------------")
+	fmt.Println("---- КОНЕЦ ----")
+}
